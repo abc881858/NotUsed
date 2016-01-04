d4b71a3e99dbdd82fb0f34b45753dca53a3ab1f2
abc881858 <abc8333@gmail.com>
2016-01-04 20:21:30 +0800

    Update main.cpp

--- DotaCard/main.cpp
+++ DotaCard/main.cpp
@@ -1,10 +1,39 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QFile>
+#include <QTextStream>
+#include <QtDebug>
 
-int main(int argc, char *argv[])
+void myMessageOutput(QtMsgType type, const QMessageLogContext& context, const QString& msg)
 {
-    QApplication a(argc, argv);
+    Q_UNUSED(context)
+    QString txt;
+    switch (type) {
+    case QtDebugMsg:
+        txt = QString("Debug: %1").arg(msg);
+        break;
+    case QtWarningMsg:
+        txt = QString("Warning: %1").arg(msg);
+        break;
+    case QtCriticalMsg:
+        txt = QString("Critical: %1").arg(msg);
+        break;
+    case QtFatalMsg:
+        txt = QString("Fatal: %1").arg(msg);
+        abort();
+    }
 
+    QFile outFile("debuglog.txt");
+    outFile.open(QIODevice::WriteOnly | QIODevice::Append);
+    QTextStream ts(&outFile);
+    ts << txt << endl;
+}
+
+int main(int argc, char* argv[])
+{
+    qInstallMessageHandler(myMessageOutput);
+
+    QApplication a(argc, argv);
     MainWindow w;
     w.show();
 
