97e9c1acb1a1db0d6fea75def5b5515a22050947
abc881858 <abc8333@gmail.com>
2016-01-04 20:24:17 +0800

    Update mainwindow.h

--- DotaCard/main.cpp
+++ DotaCard/main.cpp
@@ -21,6 +21,9 @@ void myMessageOutput(QtMsgType type, const QMessageLogContext& context, const QS
     case QtFatalMsg:
         txt = QString("Fatal: %1").arg(msg);
         abort();
+    case QtInfoMsg:
+        txt = QString("Info: %1").arg(msg);
+        break;
     }
 
     QFile outFile("debuglog.txt");
