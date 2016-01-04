f1234986f490c016fff33b54a0083bf4aaa22636
abc881858 <abc8333@gmail.com>
2016-01-04 20:21:00 +0800

    Update card.cpp

--- DotaCard/card.cpp
+++ DotaCard/card.cpp
@@ -1,6 +1,17 @@
 #include "card.h"
 
-Card::Card(QObject *parent) : QObject(parent)
+Card::Card(QObject* parent, int value)
+    : QObject(parent)
 {
+    ISDN = value;
+}
 
+int Card::getISDN() const
+{
+    return ISDN;
+}
+
+void Card::setISDN(int value)
+{
+    ISDN = value;
 }
