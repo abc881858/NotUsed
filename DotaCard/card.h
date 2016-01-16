f5f99064a8b7edc440b3ed07e8845078e805278c
abc881858 <abc8333@gmail.com>
2016-01-16 18:24:04 +0800

    Update card.h

--- DotaCard/card.h
+++ DotaCard/card.h
@@ -2,30 +2,46 @@
 #define CARD_H
 
 #include <QGraphicsObject>
+#include <QHash>
+
+const static QHash<int, QString> allnames = {
+    { 601, "dota-Lion" },
+    { 602, "dota-Rubick" },
+    { 603, "dota-Tusk" },
+    { 604, "dota-Magnus" },
+    { 605, "dota-NyxAssassin" },
+    { 606, "dota-KeeperoftheLight" },
+    { 607, "dota-CentaurWarrunner" },
+    { 608, "dota-Undying" },
+    { 609, "dota-VengefulSpirit" },
+    { 610, "dota-Zeus" }
+};
 
 class Card : public QGraphicsObject {
     Q_OBJECT
-
 public:
     explicit Card(int value = -1);
-    QString name;
-    QString description;
     int getISDN() const;
-
-    QString getPixmapPath();
 protected:
     QRectF boundingRect() const;
     void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget);
-
-    void hoverEnterEvent(QGraphicsSceneHoverEvent *);
-    void hoverLeaveEvent(QGraphicsSceneHoverEvent *);
+    void hoverEnterEvent(QGraphicsSceneHoverEvent*);
+    void hoverLeaveEvent(QGraphicsSceneHoverEvent*);
 private:
-    int ISDN;
     QPixmap pixmap;
-
+    int ISDN;
+    QString name;
+    QString area;
+    QString image;
+    QString description;
 signals:
     void hover(QString);
-public slots:
+};
+
+class Lion : public Card {
+    Q_OBJECT
+public:
+    Q_INVOKABLE Lion();
 };
 
 #endif // CARD_H
