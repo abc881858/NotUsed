d5d97f844c05f57bbc7088d85c65aa6425dd78d9
abc881858 <abc8333@gmail.com>
2016-03-31 18:58:01 +0800

    Update card.cpp

--- DotaCard/card.cpp
+++ DotaCard/card.cpp
@@ -1,6 +1,7 @@
 #include "card.h"
 #include <QPainter>
 #include <QMetaObject>
+#include <QGraphicsSceneMouseEvent>
 
 Card::Card(int value)
     : pixmap(":/png/png/NULL.jpg")
@@ -8,7 +9,7 @@ Card::Card(int value)
     setAcceptHoverEvents(true);
     setAcceptedMouseButtons(Qt::LeftButton | Qt::RightButton);
     ISDN = value;
-    name = allnames.value(ISDN);
+//    name = allnames.value(ISDN);
     area = parentObject()->metaObject()->className();
     image = QString(":/pic/monster/%1.jpg").arg(name);
 }
@@ -52,4 +53,65 @@ void Card::hoverLeaveEvent(QGraphicsSceneHoverEvent *)
     {
         setY(0);
     }
+}
+
+QString Card::getImage() const
+{
+    return image;
+}
+
+void Card::setImage(const QString &value)
+{
+    image = value;
+}
+
+QString Card::getName() const
+{
+    return name;
+}
+
+void Card::setName(const QString &value)
+{
+    name = value;
+}
+
+void Card::setISDN(int value)
+{
+    ISDN = value;
+}
+
+//void Card::mousePressEvent(QGraphicsSceneMouseEvent *event)
+//{
+//    if(event->button()==Qt::LeftButton)
+//    {
+//        emit left_click();
+//    }
+//    else if(event->button()==Qt::RightButton)
+//    {
+//        emit right_click();
+//    }
+//}
+
+Lion::Lion()
+{
+    setISDN(601);
+    setName("dota-Lion");
+    setImage(":/pic/monster/dota-Lion.jpg");
+}
+
+void Lion::mousePressEvent(QGraphicsSceneMouseEvent *event)
+{
+    if(event->button()==Qt::LeftButton)
+    {
+//        emit left_click();
+    }
+//    else if(event->button()==Qt::RightButton)
+//    {
+//        emit right_click();
+//    }
+
+//    if(area=="FieldyardArea" || area=="FieldgroundArea")
+//    {
+//        //
+//    }
 }
