3dc4a1a396c8be6fbbc2a31f7f5603f0cc13e4e8
abc881858 <abc8333@gmail.com>
2016-03-31 18:59:12 +0800

    Update mainwindow.cpp

--- DotaCard/mainwindow.cpp
+++ DotaCard/mainwindow.cpp
@@ -1,10 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QHostAddress>
-#include <QJsonDocument>
-#include <QJsonObject>
-#include <QJsonArray>
-#include <QFile>
+
 #include <QPalette>
 #include <QBrush>
 #include <QPixmap>
@@ -18,12 +14,19 @@ MainWindow::MainWindow(QWidget* parent)
     palette.setBrush(QPalette::Background, QBrush(QPixmap(":/png/png/back.jpg")));
     this->setPalette(palette);
 
-    client = new QTcpSocket(this);
-    client->connectToHost(QHostAddress::LocalHost, 7720);
-    connect(client, SIGNAL(connected()), this, SLOT(connected()));
-    connect(client, SIGNAL(readyRead()), this, SLOT(readFromServer()));
-    //  connect(client, SIGNAL(disconnected()), this, SLOT(disconnected()));
-    //  connect(client, SIGNAL(error(QAbstractSocket::SocketError)), this, SLOT(error(QAbstractSocket::SocketError)));
+    net = new Net(this);
+
+    connect(net,SIGNAL(myStartGame()),this,SLOT(startMyGame()));
+    connect(net,SIGNAL(yourStartGame()),this,SLOT(startYourGame()));
+    connect(net,SIGNAL(myDrawPhase()),this,SLOT(drawMyPhase()));
+    connect(net,SIGNAL(yourDrawPhase()),this,SLOT(drawYourPhase()));
+    connect(net,SIGNAL(myStandbyPhase()),this,SLOT(standbyMyPhase()));
+    connect(net,SIGNAL(yourStandbyPhase()),this,SLOT(standbyYourPhase()));
+    connect(net,SIGNAL(myMainPhase1()),this,SLOT(mainMyPhase1()));
+    connect(net,SIGNAL(yourMainPhase1()),this,SLOT(mainYourPhase1()));
+    connect(net,SIGNAL(yourBattlePhase()),this,SLOT(battleYourPhase()));
+    connect(net,SIGNAL(yourMainPhase2()),this,SLOT(mainYourPhase2()));
+    connect(net,SIGNAL(yourEndPhase()),this,SLOT(endYourPhase()));
 
     myLP = 8000;
     yourLP = 8000;
@@ -34,20 +37,38 @@ MainWindow::MainWindow(QWidget* parent)
     ui->roomView->setScene(roomScene);
     ui->roomView->setSceneRect(0, 0, 550, 600);
 
-    this->myDeck = roomScene->deckarea->myDeck;
-    this->myHand = roomScene->handarea->myHand;
-    this->myFieldyard = roomScene->fieldyardarea->myFieldyard;
-    this->myFieldground = roomScene->fieldgroundarea->myFieldground;
-    this->yourDeck = roomScene->enemydeckarea->yourDeck;
-    this->yourHand = roomScene->enemyhandarea->yourHand;
-    this->yourFieldyard = roomScene->enemyfieldyardarea->yourFieldyard;
-    this->yourFieldground = roomScene->enemyfieldgroundarea->yourFieldground;
+//    this->myDeck = roomScene->deckarea->myDeck;
+//    this->myHand = roomScene->handarea->myHand;
+//    this->myFieldyard = roomScene->fieldyardarea->myFieldyard;
+//    this->myFieldground = roomScene->fieldgroundarea->myFieldground;
+//    this->yourDeck = roomScene->enemydeckarea->yourDeck;
+//    this->yourHand = roomScene->enemyhandarea->yourHand;
+//    this->yourFieldyard = roomScene->enemyfieldyardarea->yourFieldyard;
+//    this->yourFieldground = roomScene->enemyfieldgroundarea->yourFieldground;
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+//void MainWindow::on_buttonBP_clicked()
+//{
+//    setPhase(myBP);
+//    client->write(getJsonFromInt(50001));
+//}
+
+//void MainWindow::on_buttonM2_clicked()
+//{
+//    setPhase(myM2);
+//    client->write(getJsonFromInt(60001));
+//}
+
+//void MainWindow::on_buttonEP_clicked()
+//{
+//    setPhase(myEP);
+//    client->write(getJsonFromInt(70001));
+//}
 
 void MainWindow::setPhase(MainWindow::Phase phase)
 {
@@ -59,247 +80,62 @@ MainWindow::Phase MainWindow::getphase() const
     return this->phase;
 }
 
-void MainWindow::connected()
-{
-    /*
- *  NOTE: this is a example for testing write QJSON to socket server!
- *  QJsonObject jsonObject;
- *  jsonObject.insert("test",QString("just tell server connected"));
- *  qDebug() << jsonObject;
- *  QJsonDocument jsonDoucment(jsonObject);
- *  QByteArray json = jsonDoucment.toJson(QJsonDocument::Compact);
- *  client->write(json);
- */
-    QFile file("test1.txt");
-    file.open(QIODevice::ReadOnly | QIODevice::Text);
-    QTextStream text_stream(&file);
-    while (!text_stream.atEnd()) {
-        myDeck << new Card(text_stream.readLine().toInt());
-    }
-    file.close();
-
-    QJsonObject jsonObject;
-    QJsonArray cards;
-    foreach (Card* card, myDeck) {
-        connect(card,SIGNAL(hover(QString)),this,SLOT(setBigImage(QString)));
-        cards << card->getISDN();
-    }
-
-    jsonObject.insert("command", 1000);
-    jsonObject.insert("cards", cards);
-
-    QJsonDocument jsonDoucment(jsonObject);
-    QByteArray json = jsonDoucment.toJson(QJsonDocument::Compact);
-    client->write(json);
-}
-
 void MainWindow::setBigImage(QString s)
 {
     ui->label->setStyleSheet(QString("image: url(%1)").arg(s));
 }
 
-void MainWindow::readFromServer()
+void MainWindow::startMyGame()
 {
-    QByteArray json = client->readAll();
-    QJsonDocument jsonDoucment = QJsonDocument::fromJson(json);
-    QJsonObject jsonObject = jsonDoucment.object();
-    qDebug() << jsonObject;
-    int command = jsonObject["command"].toInt();
-    switch (command) {
-    case 2000:
-        firstSetupEnemyDeck(jsonObject["deck"].toArray());
-        break;
-    case 3000:
-        secondSetupEnemyDeck(jsonObject["deck"].toArray());
-        break;
-    case 10000:
-        myStartGame();
-        break;
-    case 10001:
-        yourStartGame();
-        break;
-    case 10002:
-        myDrawPhase();
-        break;
-    case 20001:
-        yourDrawPhase();
-        break;
-    case 20002:
-        myStandbyPhase();
-        break;
-    case 30001:
-        yourStandbyPhase();
-        break;
-    case 30002:
-        myMainPhase1();
-        break;
-    case 40001:
-        yourMainPhase1();
-        break;
-    case 50001:
-        yourBattlePhase();
-        break;
-    case 60001:
-        yourMainPhase2();
-        break;
-    case 70001:
-        yourEndPhase();
-        break;
-    default:
-        break;
-    }
+
 }
 
-//only send int(command) to server
-QByteArray MainWindow::getJsonFromInt(int command)
+void MainWindow::startYourGame()
 {
-    QJsonObject jsonObject;
-    jsonObject.insert("command", command);
-    QJsonDocument jsonDoucment(jsonObject);
-    QByteArray json = jsonDoucment.toJson(QJsonDocument::Compact);
-    return json;
+
 }
 
-void MainWindow::firstSetupEnemyDeck(QJsonArray jsonArray)
+void MainWindow::drawMyPhase()
 {
-    foreach (const QJsonValue& value, jsonArray) {
-        int ISDN = value.toInt();
-        yourDeck << new Card(ISDN);
-    }
+
 }
 
-void MainWindow::secondSetupEnemyDeck(QJsonArray jsonArray)
+void MainWindow::drawYourPhase()
 {
-    foreach (const QJsonValue& value, jsonArray) {
-        int ISDN = value.toInt();
-        yourDeck << new Card(ISDN);
-    }
-    client->write(getJsonFromInt(1001));
+
 }
 
-void MainWindow::myStartGame()
+void MainWindow::standbyMyPhase()
 {
-    QJsonObject jsonObject;
-    QJsonArray cards;
-    for (int i = 0; i < 5; i++) {
-        Card* card = myDeck.takeFirst();
-        roomScene->handarea->addCard(card);
-        cards << card->getISDN();
-    }
-    jsonObject.insert("command", 10001);
-    jsonObject.insert("cards", cards);
-    qDebug() << jsonObject;
-    QJsonDocument jsonDoucment(jsonObject);
-    QByteArray json = jsonDoucment.toJson(QJsonDocument::Compact);
-    client->write(json);
+
 }
 
-void MainWindow::yourStartGame()
+void MainWindow::standbyYourPhase()
 {
-    for (int i = 0; i < 5; i++) {
-        Card* card = yourDeck.takeFirst();
-        roomScene->enemyhandarea->addCard(card);
-    }
 
-    QJsonObject jsonObject;
-    QJsonArray cards;
-    for (int i = 0; i < 5; i++) {
-        Card* card = myDeck.takeFirst();
-        roomScene->handarea->addCard(card);
-        cards << card->getISDN();
-    }
-    jsonObject.insert("command", 10002);
-    jsonObject.insert("cards", cards);
-    qDebug() << jsonObject;
-    QJsonDocument jsonDoucment(jsonObject);
-    QByteArray json = jsonDoucment.toJson(QJsonDocument::Compact);
-    client->write(json);
 }
 
-void MainWindow::myDrawPhase()
+void MainWindow::mainMyPhase1()
 {
-    for (int i = 0; i < 5; i++) {
-        Card* card = yourDeck.takeFirst();
-        roomScene->enemyhandarea->addCard(card);
-    }
-    setPhase(myDP);
 
-    Card* card = myDeck.takeFirst();
-    roomScene->handarea->addCard(card);
-
-    client->write(getJsonFromInt(20001));
 }
 
-void MainWindow::yourDrawPhase()
+void MainWindow::mainYourPhase1()
 {
-    Card* card = yourDeck.takeFirst();
-    roomScene->enemyhandarea->addCard(card);
 
-    setPhase(yourDP);
-    client->write(getJsonFromInt(20002));
 }
 
-void MainWindow::myStandbyPhase()
+void MainWindow::battleYourPhase()
 {
-    setPhase(mySP);
-    client->write(getJsonFromInt(30001));
+
 }
 
-void MainWindow::yourStandbyPhase()
+void MainWindow::mainYourPhase2()
 {
-    setPhase(yourSP);
-    client->write(getJsonFromInt(30002));
+
 }
 
-void MainWindow::myMainPhase1()
+void MainWindow::endYourPhase()
 {
-    setPhase(myM1);
-    client->write(getJsonFromInt(40001));
-}
 
-void MainWindow::yourMainPhase1()
-{
-    setPhase(yourM2);
-}
-
-void MainWindow::yourBattlePhase()
-{
-    setPhase(yourBP);
-}
-
-void MainWindow::yourMainPhase2()
-{
-    setPhase(yourM2);
-}
-
-void MainWindow::yourEndPhase()
-{
-    setPhase(yourEP);
-    //TODO: 也许对方结束阶段我要触发什么陷阱卡
-    //暂时玩家2直接进入他的抽卡阶段
-
-    setPhase(myDP);
-
-    Card* card = myDeck.takeFirst();
-    roomScene->handarea->addCard(card);
-
-    client->write(getJsonFromInt(20001));
-}
-
-void MainWindow::on_buttonBP_clicked()
-{
-    setPhase(myBP);
-    client->write(getJsonFromInt(50001));
-}
-
-void MainWindow::on_buttonM2_clicked()
-{
-    setPhase(myM2);
-    client->write(getJsonFromInt(60001));
-}
-
-void MainWindow::on_buttonEP_clicked()
-{
-    setPhase(myEP);
-    client->write(getJsonFromInt(70001));
 }
