9ca7d357592dc902b6f1b32f78d988cfa37be3c5
abc881858 <abc8333@gmail.com>
2016-04-22 16:34:53 +0800

    Update engine.cpp

--- DotaCard/engine.cpp
+++ DotaCard/engine.cpp
@@ -2,23 +2,32 @@
 
 Q_GLOBAL_STATIC(Engine, engine)
 
-Engine *Engine::instance()
+Engine* Engine::instance()
 {
     return engine();
 }
 
 void Engine::loadAllCards()
 {
-    allcards << new Lion();
+    allcards << new CentaurWarrunner()
+             << new KeeperoftheLight()
+             << new Lion()
+             << new Magnus()
+             << new NyxAssassin()
+             << new Rubick()
+             << new Tusk()
+             << new Undying()
+             << new VengefulSpirit()
+             << new Zeus();
 
-    foreach (Card* card, allcards)
-    {
-        metaobjects.insert(card->getISDN(),card->metaObject());
+    foreach (Card* card, allcards) {
+        metaobjects.insert(card->getISDN(), card->metaObject());
     }
 }
 
 Card* Engine::cloneCard(int ISDN)
 {
-    const QMetaObject *meta = metaobjects.value(ISDN);
-    return qobject_cast<Card *>(meta->newInstance());
+    const QMetaObject* meta = metaobjects.value(ISDN);
+    Q_ASSERT(meta != 0);
+    return qobject_cast<Card*>(meta->newInstance());
 }
