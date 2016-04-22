4d1aef04085a2a54caad7f7ba2d131e160ccd604
abc881858 <abc8333@gmail.com>
2016-04-22 16:41:46 +0800

    Update roomscene.cpp

--- DotaCard/roomscene.cpp
+++ DotaCard/roomscene.cpp
@@ -1,21 +1,14 @@
 #include "roomscene.h"
+#include "engine.h"
 
 static const QPointF DeckPos(485, 426);
 static const QPointF HandPos(19, 529);
 static const QPointF FieldyardPos(94, 317);
 static const QPointF FieldgroundPos(94, 424);
-//static const QPointF GraveyardPos(485,331);
-//static const QPointF RemovePos(479,296);
-//static const QPointF FusionPos(12,424);
-//static const QPointF EnvironmentPos(12,319);
 static const QPointF EnemyDeckPos(14, 105);
 static const QPointF EnemyHandPos(17, -71);
 static const QPointF EnemyFieldyardPos(94, 213);
 static const QPointF EnemyFieldgroundPos(94, 105);
-//static const QPointF EnemyGraveyardPos(262,196);
-//static const QPointF EnemyRemovePos(253,280);
-//static const QPointF EnemyFusionPos(742,105);
-//static const QPointF EnemyEnvironmentPos(742,209);
 
 RoomScene::RoomScene(QObject* parent)
     : QGraphicsScene(parent)
@@ -38,22 +31,6 @@ RoomScene::RoomScene(QObject* parent)
     fieldgroundarea = new FieldgroundArea;
     addItem(fieldgroundarea);
     fieldgroundarea->setPos(FieldgroundPos);
-    //    // create GraveyardArea
-    //    graveyardarea = new GraveyardArea;
-    //    addItem(graveyardarea);
-    //    graveyardarea->setPos(GraveyardPos);
-    //    // create RemoveArea
-    //    removearea = new RemoveArea;
-    //    addItem(removearea);
-    //    removearea->setPos(RemovePos);
-    //    // create FusionArea
-    //    fusionarea = new FusionArea;
-    //    addItem(fusionarea);
-    //    fusionarea->setPos(FusionPos);
-    //    // create EnvironmentArea
-    //    environmentarea = new EnvironmentArea;
-    //    addItem(environmentarea);
-    //    environmentarea->setPos(EnvironmentPos);
 
     // create Enemy
     enemydeckarea = new EnemyDeckArea;
@@ -68,16 +45,65 @@ RoomScene::RoomScene(QObject* parent)
     enemyfieldgroundarea = new EnemyFieldgroundArea;
     addItem(enemyfieldgroundarea);
     enemyfieldgroundarea->setPos(EnemyFieldgroundPos);
-    //    enemygraveyardarea = new EnemyGraveyardArea;
-    //    addItem(enemygraveyardarea);
-    //    enemygraveyardarea->setPos(EnemyGraveyardPos);
-    //    enemyremovearea = new EnemyRemoveArea;
-    //    addItem(enemyremovearea);
-    //    enemyremovearea->setPos(EnemyRemovePos);
-    //    enemyfusionarea = new EnemyFusionArea;
-    //    addItem(enemyfusionarea);
-    //    enemyfusionarea->setPos(EnemyFusionPos);
-    //    enemyenvironmentarea = new EnemyEnvironmentArea;
-    //    addItem(enemyenvironmentarea);
-    //    enemyenvironmentarea->setPos(EnemyEnvironmentPos);
+}
+
+void RoomScene::setupDeck(QList<int> list)
+{
+    foreach (int ISDN, list) {
+        Card* card = Engine::instance()->cloneCard(ISDN);
+        deckarea->addCard(card);
+        connect(card, SIGNAL(hover(QString)), this, SIGNAL(hoverCard(QString)));
+    }
+}
+
+void RoomScene::setupEnemyDeck(QList<int> list)
+{
+    foreach (int ISDN, list) {
+        Card* card = Engine::instance()->cloneCard(ISDN);
+        enemydeckarea->addCard(card);
+    }
+}
+
+QList<int> RoomScene::startMyGame()
+{
+    QList<int> list;
+    for (int i = 0; i < 5; i++) {
+        Card* card = deckarea->takeFirstCard();
+        handarea->addCard(card);
+        list << card->getISDN();
+    }
+    return list;
+}
+
+QList<int> RoomScene::startYourGame()
+{
+    for (int i = 0; i < 5; i++) {
+        Card* card = enemydeckarea->takeFirstCard();
+        enemyhandarea->addCard(card);
+    }
+
+    return startMyGame();
+}
+
+void RoomScene::drawMyPhase()
+{
+    for (int i = 0; i < 5; i++) {
+        Card* card = enemydeckarea->takeFirstCard();
+        enemyhandarea->addCard(card);
+    }
+
+    Card* card = deckarea->takeFirstCard();
+    handarea->addCard(card);
+}
+
+void RoomScene::drawYourPhase()
+{
+    Card* card = enemydeckarea->takeFirstCard();
+    enemyhandarea->addCard(card);
+}
+
+void RoomScene::endYourPhase()
+{
+    Card* card = deckarea->takeFirstCard();
+    handarea->addCard(card);
 }
