9c2cdf5bd8215262a09b781974151419a04a9b21
abc881858 <abc8333@gmail.com>
2016-04-22 16:38:42 +0800

    Update net.cpp

--- DotaCard/net.cpp
+++ DotaCard/net.cpp
@@ -5,44 +5,31 @@
 #include <QJsonArray>
 #include <QFile>
 
-Net::Net(QObject *parent) : QObject(parent)
+Net::Net(QObject* parent)
+    : QObject(parent)
 {
     client = new QTcpSocket(this);
     client->connectToHost(QHostAddress::LocalHost, 7720);
     connect(client, SIGNAL(connected()), this, SLOT(connected()));
     connect(client, SIGNAL(readyRead()), this, SLOT(readFromServer()));
-    //  connect(client, SIGNAL(disconnected()), this, SLOT(disconnected()));
-    //  connect(client, SIGNAL(error(QAbstractSocket::SocketError)), this, SLOT(error(QAbstractSocket::SocketError)));
 }
 
 void Net::connected()
 {
-    /*
- *  NOTE: this is a example for testing write QJSON to socket server!
- *  QJsonObject jsonObject;
- *  jsonObject.insert("test",QString("just tell server connected"));
- *  qDebug() << jsonObject;
- *  QJsonDocument jsonDoucment(jsonObject);
- *  QByteArray json = jsonDoucment.toJson(QJsonDocument::Compact);
- *  client->write(json);
- */
-    QList<Card*> myDeck;
+    QJsonArray cards;
+    QList<int> myDeck;
     QFile file("test1.txt");
     file.open(QIODevice::ReadOnly | QIODevice::Text);
     QTextStream text_stream(&file);
     while (!text_stream.atEnd()) {
-        myDeck << new Card(text_stream.readLine().toInt());
+        int ISDN = text_stream.readLine().toInt();
+        myDeck << ISDN;
+        cards << ISDN;
     }
     file.close();
-    emit loadDeck(myDeck);
+    emit setupDeck(myDeck);
 
     QJsonObject jsonObject;
-    QJsonArray cards;
-    foreach (Card* card, myDeck) {
-        connect(card,SIGNAL(hover(QString)),this,SLOT(setBigImage(QString)));
-        cards << card->getISDN();
-    }
-
     jsonObject.insert("command", 1000);
     jsonObject.insert("cards", cards);
 
@@ -103,7 +90,6 @@ void Net::readFromServer()
     }
 }
 
-//only send int(command) to server
 QByteArray Net::getJsonFromInt(int command)
 {
     QJsonObject jsonObject;
@@ -115,10 +101,10 @@ QByteArray Net::getJsonFromInt(int command)
 
 void Net::firstSetupEnemyDeck(QJsonArray jsonArray)
 {
-    QList<Card*> yourDeck;
+    QList<int> yourDeck;
     foreach (const QJsonValue& value, jsonArray) {
         int ISDN = value.toInt();
-        yourDeck << new Card(ISDN);
+        yourDeck << ISDN;
     }
     emit setupEnemyDeck(yourDeck);
 }
