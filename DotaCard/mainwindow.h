303b829187e1f10c2bf1e20fe32f2f08148eeb93
吴冬亮 <abc8333@gmail.com>
2016-08-30 18:51:45 +0800

    Update mainwindow.h

--- DotaCard/mainwindow.h
+++ DotaCard/mainwindow.h
@@ -5,37 +5,20 @@
 #include <QList>
 #include "roomscene.h"
 
-namespace Ui {
+namespace Ui
+{
 class MainWindow;
 }
 
-class MainWindow : public QMainWindow {
+class MainWindow : public QMainWindow
+{
     Q_OBJECT
-    Q_ENUMS(Phase)
-
 public:
     explicit MainWindow(QWidget* parent = 0);
     ~MainWindow();
 
-    enum Phase { myDP,
-        mySP,
-        myM1,
-        myBP,
-        myM2,
-        myEP,
-        yourDP,
-        yourSP,
-        yourM1,
-        yourBP,
-        yourM2,
-        yourEP };
-    void setPhase(Phase phase);
-    Phase getphase() const;
-
 private:
     Ui::MainWindow* ui;
-    Phase phase;
-
     int myLP;
     int yourLP;
 
