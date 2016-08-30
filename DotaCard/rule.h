0ff1129c4484ec1f572b20a9ddbae83a0f461563
吴冬亮 <abc8333@gmail.com>
2016-08-30 18:53:47 +0800

    Update rule.h

--- DotaCard/rule.h
+++ DotaCard/rule.h
@@ -1,17 +1,36 @@
 #ifndef RULE_H
 #define RULE_H
 
-#include "card.h"
-
-class Rule {
+class Rule
+{
 public:
+    enum Phase
+    {
+        myDP,
+        mySP,
+        myM1,
+        myBP,
+        myM2,
+        myEP,
+        yourDP,
+        yourSP,
+        yourM1,
+        yourBP,
+        yourM2,
+        yourEP
+    };
+
     static Rule* instance();
 
     bool getOneTurnOneNormalSummon() const;
     void setOneTurnOneNormalSummon(bool value);
 
+    void setPhase(Phase phase);
+    Phase getphase() const;
+
 private:
     bool oneTurnOneNormalSummon;
+    Rule::Phase phase;
 };
 
 #endif // RULE_H
