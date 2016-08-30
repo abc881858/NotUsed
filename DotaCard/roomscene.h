b34d8042af8c81a29766db5ced73607118623d24
吴冬亮 <abc8333@gmail.com>
2016-08-30 18:52:57 +0800

    Update roomscene.h

--- DotaCard/roomscene.h
+++ DotaCard/roomscene.h
@@ -3,8 +3,11 @@
 
 #include <QGraphicsScene>
 #include "area.h"
+#include <QMenu>
+#include <QAction>
 
-class RoomScene : public QGraphicsScene {
+class RoomScene : public QGraphicsScene
+{
     Q_OBJECT
 public:
     explicit RoomScene(QObject* parent = 0);
@@ -17,7 +20,9 @@ public:
     void setupEnemyDeck(QList<int> list);
 
     void initializeFieldyard();
-    void doActionCommand(int parameter, int from, int to);
+
+    void doActionCommand(int parameter, int from, int);
+
 private:
     DeckArea* deckarea;
     HandArea* handarea;
@@ -28,11 +33,20 @@ private:
     EnemyFieldyardArea* enemyfieldyardarea;
     EnemyFieldgroundArea* enemyfieldgroundarea;
 
+    QMenu* myContextMenu;
+    QAction* goBP;
+    QAction* goM2;
+    QAction* goEP;
+
 signals:
     void hover(QString);
 
 public slots:
-
+    void actionBP(bool);
+    void actionM2(bool);
+    void actionEP(bool);
+protected:
+    void contextMenuEvent(QGraphicsSceneContextMenuEvent *event);
 };
 
 #endif // ROOMSCENE_H
