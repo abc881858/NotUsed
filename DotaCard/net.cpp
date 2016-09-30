ad80da808f9cb9088798c581eac227f8623b6147
吴冬亮 <abc8333@gmail.com>
2016-09-30 17:36:38 +0800

    Update net.cpp

--- DotaCard/net.cpp
+++ DotaCard/net.cpp
@@ -4,6 +4,7 @@
 #include <QJsonObject>
 #include <QJsonArray>
 #include <QFile>
+#include <functional>
 
 Q_GLOBAL_STATIC(Net, net)
 
@@ -18,6 +19,36 @@ void Net::initialize()
     client->connectToHost(QHostAddress::LocalHost, 7720);
     connect(client, SIGNAL(connected()), this, SLOT(connected()));
     connect(client, SIGNAL(readyRead()), this, SLOT(readFromServer()));
+}
+
+void Net::write(QJsonObject jsonObject)
+{
+    QJsonDocument jsonDoucment(jsonObject);
+    QByteArray byteArray = jsonDoucment.toJson(QJsonDocument::Compact); //压缩的json
+    client->write(byteArray);
+}
+
+void Net::doAddCard(int ISDN, int area, bool face, bool stand) //添加
+{
+    QJsonObject jsonPara;
+    jsonPara.insert("ISDN", ISDN);
+    jsonPara.insert("area", area);
+    jsonPara.insert("face", face);
+    jsonPara.insert("stand", stand);
+    QJsonObject jsonObject;
+    jsonObject.insert("cmd", "doAddCard");
+    jsonObject.insert("parameter", jsonPara);
+    write(jsonObject);
+}
+
+void Net::doTakeCard(int ISDN) //删除
+{
+    QJsonObject jsonPara;
+    jsonPara.insert("ISDN", ISDN);
+    QJsonObject jsonObject;
+    jsonObject.insert("cmd", "doTakeCard");
+    jsonObject.insert("parameter", jsonPara);
+    write(jsonObject);
 }
 
 void Net::connected()
@@ -39,10 +70,7 @@ void Net::connected()
     QJsonObject jsonObject;
     jsonObject.insert("command", 1000);
     jsonObject.insert("cards", cards);
-
-    QJsonDocument jsonDoucment(jsonObject);
-    QByteArray json = jsonDoucment.toJson(QJsonDocument::Compact);
-    client->write(json);
+    write(jsonObject);
 }
 
 void Net::readFromServer()
@@ -51,164 +79,163 @@ void Net::readFromServer()
     QJsonDocument jsonDoucment = QJsonDocument::fromJson(json);
     QJsonObject jsonObject = jsonDoucment.object();
     qDebug() << "Net's readFromServer" << jsonObject;
-    int command = jsonObject["command"].toInt();
-    switch (command)
-    {
-    case 2000:
-        firstSetupEnemyDeck(jsonObject["deck"].toArray());
-        break;
-    case 3000:
-        secondSetupEnemyDeck(jsonObject["deck"].toArray());
-        break;
-    case 10000:
-        emit myStartGame();
-        break;
-    case 10001:
-        emit yourStartGame();
-        break;
-    case 10002:
-        emit myDrawPhase();
-        break;
-    case 20001:
-        emit yourDrawPhase();
-        break;
-    case 20002:
-        emit myStandbyPhase();
-        break;
-    case 30001:
-        emit yourStandbyPhase();
-        break;
-    case 30002:
-        emit myMainPhase1();
-        break;
-    case 40001:
-        emit yourMainPhase1();
-        break;
-    case 50001:
-        emit yourBattlePhase();
-        break;
-    case 60001:
-        emit yourMainPhase2();
-        break;
-    case 70001:
-        emit yourEndPhase();
-        break;
-    case 8888:
-        emit actionCommand(jsonObject["parameter"].toInt(), jsonObject["from"].toInt());
-        break;
-    case 0: //说明对方不采取任何行动，我方继续行动
-        emit getResponse();
-    default:
-        break;
-    }
+
+    QString cmd = "cmd_";
+    cmd.append(jsonObject["command"].toString());
+    QJsonObject jsonPara = jsonObject["parameter"].toObject();
+
+    QMetaObject::invokeMethod(this, cmd.toLatin1(), Q_ARG(QJsonObject, jsonPara));
+
+
+
+    //    int command = jsonObject["command"].toInt();
+    //    switch (command)
+    //    {
+    //    case 2000:
+    //        firstSetupEnemyDeck(jsonObject["deck"].toArray());
+    //        break;
+    //    case 3000:
+    //        secondSetupEnemyDeck(jsonObject["deck"].toArray());
+    //        break;
+    //    case 10000:
+    //        emit myStartGame();
+    //        break;
+    //    case 10001:
+    //        emit yourStartGame();
+    //        break;
+    //    case 10002:
+    //        emit myDrawPhase();
+    //        break;
+    //    case 20001:
+    //        emit yourDrawPhase();
+    //        break;
+    //    case 20002:
+    //        emit myStandbyPhase();
+    //        break;
+    //    case 30001:
+    //        emit yourStandbyPhase();
+    //        break;
+    //    case 30002:
+    //        emit myMainPhase1();
+    //        break;
+    //    case 40001:
+    //        emit yourMainPhase1();
+    //        break;
+    //    case 50001:
+    //        emit yourBattlePhase();
+    //        break;
+    //    case 60001:
+    //        emit yourMainPhase2();
+    //        break;
+    //    case 70001:
+    //        emit yourEndPhase();
+    //        break;
+    //    case 8888:
+    //        emit actionCommand(jsonObject["parameter"].toInt(), jsonObject["from"].toInt());
+    //        break;
+    //    case 0: //说明对方不采取任何行动，我方继续行动
+    //        emit getResponse();
+    //    default:
+    //        break;
+    //    }
 }
 
-QByteArray Net::getJsonFromInt(int command)
-{
-    QJsonObject jsonObject;
-    jsonObject.insert("command", command);
-    QJsonDocument jsonDoucment(jsonObject);
-    QByteArray json = jsonDoucment.toJson(QJsonDocument::Compact);
-    return json;
-}
+//QByteArray Net::getJsonFromInt(int command)
+//{
+//    QJsonObject jsonObject;
+//    jsonObject.insert("command", command);
+//    return jsonObject;
+//}
 
-void Net::firstSetupEnemyDeck(QJsonArray jsonArray)
-{
-    QList<int> yourDeck;
-    foreach (const QJsonValue& value, jsonArray)
-    {
-        int ISDN = value.toInt();
-        yourDeck << ISDN;
-    }
-    emit setupEnemyDeck(yourDeck);
-}
+//void Net::firstSetupEnemyDeck(QJsonArray jsonArray)
+//{
+//    QList<int> yourDeck;
+//    foreach (const QJsonValue& value, jsonArray)
+//    {
+//        int ISDN = value.toInt();
+//        yourDeck << ISDN;
+//    }
+//    emit setupEnemyDeck(yourDeck);
+//}
 
-void Net::secondSetupEnemyDeck(QJsonArray jsonArray)
-{
-    firstSetupEnemyDeck(jsonArray);
-    sendMessage(1001);
-}
+//void Net::secondSetupEnemyDeck(QJsonArray jsonArray)
+//{
+//    firstSetupEnemyDeck(jsonArray);
+//    sendMessage(1001);
+//}
 
-void Net::sendMessage(int command)
-{
-    client->write(getJsonFromInt(command));
-}
+//void Net::sendMessage(int command)
+//{
+//    client->write(getJsonFromInt(command));
+//}
 
-void Net::sendMessage(int command, QList<int> list)
-{
-    QJsonObject jsonObject;
-    QJsonArray cards;
-    foreach (int card, list)
-    {
-        cards << QJsonValue(card);
-    }
+//void Net::sendMessage(int command, QList<int> list)
+//{
+//    QJsonObject jsonObject;
+//    QJsonArray cards;
+//    foreach (int card, list)
+//    {
+//        cards << QJsonValue(card);
+//    }
 
-    jsonObject.insert("command", command);
-    jsonObject.insert("cards", cards);
-    QJsonDocument jsonDoucment(jsonObject);
-    QByteArray json = jsonDoucment.toJson(QJsonDocument::Compact);
-    client->write(json);
-}
+//    jsonObject.insert("command", command);
+//    jsonObject.insert("cards", cards);
+//    write(jsonObject);
+//}
 
-void Net::doNormalSummon(int index)
-{
-    qDebug() << "Net's doNormalSummon: from = " << index;
+//void Net::doNormalSummon(int index)
+//{
+//    qDebug() << "Net's doNormalSummon: from = " << index;
 
-    emit actionCommand(99991, index);
+//    emit actionCommand(99991, index);
 
-    QJsonObject jsonObject;
-    jsonObject.insert("command", 8888);
-    jsonObject.insert("parameter", 88881);
-    jsonObject.insert("index", index);
-    QJsonDocument jsonDoucment(jsonObject);
-    QByteArray json = jsonDoucment.toJson(QJsonDocument::Compact);
-    client->write(json);
-}
+//    QJsonObject jsonObject;
+//    jsonObject.insert("command", 8888);
+//    jsonObject.insert("parameter", 88881);
+//    jsonObject.insert("index", index);
+//    write(jsonObject);
+//}
 
-void Net::doSetCard(int index)
-{
-    qDebug() << "Net's doSetCard: index = " << index;
-}
+//void Net::doSetCard(int index)
+//{
+//    qDebug() << "Net's doSetCard: index = " << index;
+//}
 
-void Net::doEffect(int index)
-{
-    qDebug() << "Net's doEffect: index = " << index;
-}
+//void Net::doEffect(int index)
+//{
+//    qDebug() << "Net's doEffect: index = " << index;
+//}
 
-void Net::doSpecialSummon(int index)
-{
-    qDebug() << "Net's doSpecialSummon: index = " << index;
-}
+//void Net::doSpecialSummon(int index)
+//{
+//    qDebug() << "Net's doSpecialSummon: index = " << index;
+//}
 
-void Net::doTribute(int index)
-{
-    qDebug() << "Net's doTribute: index = " << index;
+//void Net::doTribute(int index)
+//{
+//    qDebug() << "Net's doTribute: index = " << index;
 
-    emit actionCommand(99992, index);
+//    emit actionCommand(99992, index);
 
-    QJsonObject jsonObject;
-    jsonObject.insert("command", 8888);
-    jsonObject.insert("parameter", 88882);
-    jsonObject.insert("index", index);
-    QJsonDocument jsonDoucment(jsonObject);
-    QByteArray json = jsonDoucment.toJson(QJsonDocument::Compact);
-    client->write(json);
-}
+//    QJsonObject jsonObject;
+//    jsonObject.insert("command", 8888);
+//    jsonObject.insert("parameter", 88882);
+//    jsonObject.insert("index", index);
+//    write(jsonObject);
+//}
 
-void Net::doEndOpponentBattlePhase()
-{
-    //
-}
+//void Net::doEndOpponentBattlePhase()
+//{
+//    //
+//}
 
-void Net::doCentaurWarrunnerEffect2()
-{
-    emit actionCommand(99993, index);
+//void Net::doCentaurWarrunnerEffect2()
+//{
+//    emit actionCommand(99993, index);
 
-    QJsonObject jsonObject;
-    jsonObject.insert("command", 8888);
-    jsonObject.insert("parameter", 88883);
-    jsonObject.insert("index", index);
-    QJsonDocument jsonDoucment(jsonObject);
-    QByteArray json = jsonDoucment.toJson(QJsonDocument::Compact);
-    client->write(json);
-}
+//    QJsonObject jsonObject;
+//    jsonObject.insert("command", 8888);
+//    jsonObject.insert("parameter", 88883);
+//    jsonObject.insert("index", index);
+//    write(jsonObject);
+//}
