75b8f67b5c56201dee7f3778f0a949fea7b69957
吴冬亮 <abc8333@gmail.com>
2016-10-07 00:05:43 +0800

    Update net.h

--- DotaCard/net.h
+++ DotaCard/net.h
@@ -2,6 +2,10 @@
 #define NET_H
 
 #include <QTcpSocket>
+#include <QJsonDocument>
+#include <QJsonValue>
+#include <QJsonArray>
+#include <QJsonObject>
 
 /*!
  * \brief The Net class
@@ -16,21 +20,8 @@ public:
 
     void initialize();
     void write(QJsonObject jsonObject);
-    void doAddCard(int ISDN, int area, bool face, bool stand);
-    void doTakeCard(int ISDN);
-
-//    QByteArray getJsonFromInt(int command);
-//    void firstSetupEnemyDeck(QJsonArray jsonArray);
-//    void secondSetupEnemyDeck(QJsonArray jsonArray);
-//    void sendMessage(int command, QList<int> list);
-//    void sendMessage(int command);
-//    void doNormalSummon(int index);
-//    void doSetCard(int index);
-//    void doEffect(int index);
-//    void doSpecialSummon(int index);
-//    void doTribute(int index);
-//    void doEndOpponentBattlePhase();
-//    void doCentaurWarrunnerEffect2();
+    void doAddCard(int ISDN, int area, int index, bool face, bool stand);
+    void doTakeCard(int area, int index);
 
 private:
     QTcpSocket* client;
@@ -40,30 +31,11 @@ public slots:
     void connected();
 
 signals:
-    Q_INVOKABLE void cmd_doAddCard(QJsonObject);
-    Q_INVOKABLE void cmd_doTakeCard(QJsonObject);
-
-//    void setupDeck(QList<int> myDeck);
-//    void setupEnemyDeck(QList<int> yourDeck);
-//    void myStartGame();
-//    void yourStartGame();
-//    void myDrawPhase();
-//    void yourDrawPhase();
-//    void myStandbyPhase();
-//    void yourStandbyPhase();
-//    void myMainPhase1();
-//    void yourMainPhase1();
-//    void yourBattlePhase();
-//    void yourMainPhase2();
-//    void yourEndPhase();
-
-//    //【commmand 8888】 【from】
-//    // 0 卡组，1-20 手牌，100-104 前场，105-109 后场
-//    // 200 墓地，300 除外区域，400 融合额外区域
-//    // 1000 对方卡组，1001-1020 对方手牌，1100-1104 对方前场，1105-1109 对方后场
-//    // 1200 对方墓地，1300 对方除外区域，1400 对方融合额外区域
-//    void actionCommand(int, int);
-//    void getResponse();
+    void setupDeck(QList<int>);
+    void setupEnemyDeck(QList<int>);
+    void startMyGame();
+    Q_INVOKABLE void request_doAddCard(QJsonObject);
+    Q_INVOKABLE void request_doTakeCard(QJsonObject);
 };
 
 #endif // NET_H
