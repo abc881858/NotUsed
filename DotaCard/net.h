7520e018c0aae0c299abf1ebbb23ba6016b7ed08
吴冬亮 <abc8333@gmail.com>
2016-10-08 18:01:25 +0800

    Update net.h

--- DotaCard/net.h
+++ DotaCard/net.h
@@ -7,10 +7,6 @@
 #include <QJsonArray>
 #include <QJsonObject>
 
-/*!
- * \brief The Net class
- */
-
 class Net : public QObject
 {
     Q_OBJECT
@@ -23,6 +19,8 @@ public:
     void doAddCard(int ISDN, int area, int index, bool face, bool stand);
     void doTakeCard(int area, int index);
 
+    void sendMessage(int command);
+    void doSetPhase(int phase);
 private:
     QTcpSocket* client;
 
@@ -32,10 +30,17 @@ public slots:
 
 signals:
     void setupDeck(QList<int>);
-    void setupEnemyDeck(QList<int>);
-    void startMyGame();
+
     Q_INVOKABLE void request_doAddCard(QJsonObject);
     Q_INVOKABLE void request_doTakeCard(QJsonObject);
+    Q_INVOKABLE void request_doSetPhase(QJsonObject);
+    Q_INVOKABLE void request_startGame();
+    Q_INVOKABLE void request_drawPhase();
+    Q_INVOKABLE void request_standbyPhase();
+    Q_INVOKABLE void request_main1Phase();
+    Q_INVOKABLE void request_battlePhase();
+    Q_INVOKABLE void request_main2Phase();
+    Q_INVOKABLE void request_endPhase();
 };
 
 #endif // NET_H
