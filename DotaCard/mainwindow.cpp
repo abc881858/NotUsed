9fa9edbe48852370b79487ecc773c55a00a46572
吴冬亮 <abc8333@gmail.com>
2016-10-13 20:40:12 +0800

    Update mainwindow.cpp

--- DotaCard/mainwindow.cpp
+++ DotaCard/mainwindow.cpp
@@ -20,10 +20,6 @@ MainWindow::MainWindow(QWidget* parent)
     Net::instance()->initialize();
     Rule::instance()->initialize();
 
-    //    //对端做了什么操作，我方都要显示出来
-    //    connect(Net::instance(), SIGNAL(actionCommand(int, int)), this, SLOT(doActionCommand(int, int)));
-    //    connect(Net::instance(), SIGNAL(getResponse()), this, SLOT(doGetResponse()));
-
     myLP = 8000;
     yourLP = 8000;
     ui->myLcdNumber->display(myLP);
@@ -33,11 +29,27 @@ MainWindow::MainWindow(QWidget* parent)
     ui->roomView->setScene(roomScene);
     ui->roomView->setSceneRect(0, 0, 550, 600);
 
-    connect(roomScene, &RoomScene::hover, [=](QString name)
+    connect(roomScene, &RoomScene::hover, [=](QString name, QString description)
         {
             //首先一张卡被hover，左上角大图肯定要更新
             //但对方的（除前后场的face-up卡）不该显示，我方卡组也不该显示
             ui->label->setStyleSheet(QString("image: url(:/pic/monster/%1.jpg)").arg(name));
+            ui->textEdit->setText(description);
+        });
+    connect(ui->buttonBP, &QPushButton::pressed, [=]()
+        {
+            Rule::instance()->setPhase(Rule::myBP);
+            Rule::instance()->setDoing(false);
+        });
+    connect(ui->buttonM2, &QPushButton::pressed, [=]()
+        {
+            Rule::instance()->setPhase(Rule::myM2);
+            Rule::instance()->setDoing(false);
+        });
+    connect(ui->buttonEP, &QPushButton::pressed, [=]()
+        {
+            Rule::instance()->setPhase(Rule::myEP);
+            Rule::instance()->setDoing(false);
         });
 }
 
@@ -45,32 +57,3 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
-
-//void MainWindow::battleYourPhase()
-//{
-//    Rule::instance()->setPhase(Rule::yourBP);
-//    //如果前后场都没有卡，就显示对话框了
-//    QMessageBox msgBox(this);
-//    msgBox.setText("dui fang jin ru zhan dou liu cheng.");
-//    msgBox.setInformativeText("qing wen fa dong ka pian te xiao ma?");
-//    msgBox.setStandardButtons(QMessageBox::Ok | QMessageBox::Cancel);
-//    int ret = msgBox.exec();
-//    if (ret == QMessageBox::Close)
-//    {
-//        //        Net::instance()->sendMessage(0);
-//    }
-//    else
-//    {
-//        Rule::instance()->setIsResponsing(true);
-//    }
-//}
-
-//void MainWindow::doGetResponse()
-//{
-//    Rule::instance()->setIsWaiting(false);
-//}
-
-//void MainWindow::doActionCommand(int parameter, int index)
-//{
-//    roomScene->doActionCommand(parameter, index);
-//}
