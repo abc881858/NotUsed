115456730ddf675dd3adf28d12cfe5ade6df7235
吴冬亮 <abc8333@gmail.com>
2016-10-13 11:11:49 +0800

    Create clear_debug_log.bat

---
+++ clear_debug_log.bat
@@ -0,0 +1,8 @@
+@echo off
+cd "C:\Qt\dotaCardwc\DotaCardDuel-master\DotaCardServer\debug"
+cd.> debuglog.txt
+cd "C:\Qt\dotaCardwc\DotaCardDuel-master\DotaCard\debug"
+cd.> debuglog.txt
+cd "C:\Qt\dotaCardwc\DotaCardDuel-master\debug2"
+cd.> debuglog.txt
+exit
