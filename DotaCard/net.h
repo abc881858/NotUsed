ee163467513fb30c7287105d9ec84e1050b5e717
吴冬亮 <abc8333@gmail.com>
2016-10-13 20:53:44 +0800

    Update net.h

--- DotaCard/net.h
+++ DotaCard/net.h
@@ -15,10 +15,10 @@ public:
     void write(QJsonObject jsonObject);
     void doAddCard(int ISDN, int area, int index, bool face, bool stand);
     void doTakeCard(int area, int index);
+    void doCentaurWarrunnerEffect(bool all);
 
     void sendMessage(int command);
     void doSetPhase(int phase);
-    void doEndOpponentBattlePhase();
 private:
     QWebSocket* client;
 
@@ -37,6 +37,16 @@ signals:
     Q_INVOKABLE void request_main1Phase();
     Q_INVOKABLE void request_doEndOpponentBattlePhase();
     Q_INVOKABLE void request_askForResponse();
+    Q_INVOKABLE void request_doCentaurWarrunnerEffect(QJsonObject);
+    Q_INVOKABLE void request_doKeeperoftheLightEffect(QJsonObject);
+    Q_INVOKABLE void request_doLionEffect(QJsonObject);
+    Q_INVOKABLE void request_doMagnusEffect(QJsonObject);
+    Q_INVOKABLE void request_doNyxAssassinEffect(QJsonObject);
+    Q_INVOKABLE void request_doRubickEffect(QJsonObject);
+    Q_INVOKABLE void request_doTuskEffect(QJsonObject);
+    Q_INVOKABLE void request_doUndyingEffect(QJsonObject);
+    Q_INVOKABLE void request_doVengefulSpiritEffect(QJsonObject);
+    Q_INVOKABLE void request_doZeusEffect(QJsonObject);
 };
 
 #endif // NET_H
