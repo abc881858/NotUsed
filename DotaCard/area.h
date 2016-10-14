a29e5f870f1d0f1ba7880bd04d8a9335cdf0ae85
吴冬亮 <abc8333@gmail.com>
2016-10-14 17:41:23 +0800

    Update area.h

--- DotaCard/area.h
+++ DotaCard/area.h
@@ -1,51 +1,42 @@
 #ifndef AREA_H
 #define AREA_H
 
-#include <QGraphicsObject>
+#include <QGraphicsPixmapItem>
 
 class Card;
 
-class DeckArea : public QGraphicsObject
+class DeckArea : public QObject, public QGraphicsPixmapItem
 {
     Q_OBJECT
 public:
-    DeckArea();
-    QRectF boundingRect() const;
-    void paint(QPainter* painter, const QStyleOptionGraphicsItem*, QWidget*);
+    static DeckArea* instance();
     void addCard(Card* card);
     Card* takeCard(int index = 0);
     QList<Card*> getMyDeck() const;
 
 private:
-    QPixmap pixmap;
     QList<Card*> myDeck;
 };
 
-class HandArea : public QGraphicsObject
+class HandArea : public QObject, public QGraphicsPixmapItem
 {
     Q_OBJECT
 public:
-    HandArea();
-    QRectF boundingRect() const;
-    void paint(QPainter* painter, const QStyleOptionGraphicsItem*, QWidget*);
+    static HandArea* instance();
     void adjustCards();
     void addCard(Card* card);
     Card* takeCard(int index);
     QList<Card*> getMyHand() const;
 
 private:
-    QPixmap pixmap;
     QList<Card*> myHand;
 };
 
-class FieldyardArea : public QGraphicsObject
+class FieldyardArea : public QObject, public QGraphicsPixmapItem
 {
     Q_OBJECT
 public:
-    FieldyardArea();
-    QRectF boundingRect() const;
-    void paint(QPainter* painter, const QStyleOptionGraphicsItem*, QWidget*);
-    //    void refreshCardFlags();
+    static FieldyardArea* instance();
     void initializeCards();
     void adjustCards();
     void addCard(Card* card, bool face = true, bool stand = true);
@@ -53,134 +44,106 @@ public:
     QList<Card*> getMyFieldyard() const;
 
 private:
-    QPixmap pixmap;
     QList<Card*> myFieldyard;
 };
 
-class FieldgroundArea : public QGraphicsObject
+class FieldgroundArea : public QObject, public QGraphicsPixmapItem
 {
     Q_OBJECT
 public:
-    FieldgroundArea();
-    QRectF boundingRect() const;
-    void paint(QPainter* painter, const QStyleOptionGraphicsItem*, QWidget*);
+    static FieldgroundArea* instance();
     QList<Card*> getMyFieldground() const;
 
 private:
-    QPixmap pixmap;
     QList<Card*> myFieldground;
 };
 
-class GraveyardArea : public QGraphicsObject
+class GraveyardArea : public QObject, public QGraphicsPixmapItem
 {
     Q_OBJECT
 public:
-    GraveyardArea();
-    QRectF boundingRect() const;
-    void paint(QPainter* painter, const QStyleOptionGraphicsItem*, QWidget*);
+    static GraveyardArea* instance();
     void adjustCards();
     void addCard(Card* card);
     Card* takeCard(int index);
     QList<Card*> getMyGraveyard() const;
 
 private:
-    QPixmap pixmap;
     QList<Card*> myGraveyard;
 };
 
-class EnemyDeckArea : public QGraphicsObject
+class EnemyDeckArea : public QObject, public QGraphicsPixmapItem
 {
     Q_OBJECT
 public:
-    EnemyDeckArea();
-    QRectF boundingRect() const;
-    void paint(QPainter* painter, const QStyleOptionGraphicsItem*, QWidget*);
+    static EnemyDeckArea* instance();
     void addCard(Card* card);
     Card* takeCard(int index = 0);
     QList<Card*> getYourDeck() const;
-
     void response_addCard(Card* card);
     Card* response_takeCard(int index);
 
 private:
-    QPixmap pixmap;
     QList<Card*> yourDeck;
 };
 
-class EnemyHandArea : public QGraphicsObject
+class EnemyHandArea : public QObject, public QGraphicsPixmapItem
 {
     Q_OBJECT
 public:
-    EnemyHandArea();
-    QRectF boundingRect() const;
-    void paint(QPainter* painter, const QStyleOptionGraphicsItem*, QWidget*);
+    static EnemyHandArea* instance();
     void adjustCards();
     void addCard(Card* card);
     Card* takeCard(int index);
     QList<Card*> getYourHand() const;
-
     void response_addCard(Card* card);
     Card* response_takeCard(int index);
 
 private:
-    QPixmap pixmap;
     QList<Card*> yourHand;
 };
 
-class EnemyFieldyardArea : public QGraphicsObject
+class EnemyFieldyardArea : public QObject, public QGraphicsPixmapItem
 {
     Q_OBJECT
 public:
-    EnemyFieldyardArea();
-    QRectF boundingRect() const;
-    void paint(QPainter* painter, const QStyleOptionGraphicsItem*, QWidget*);
+    static EnemyFieldyardArea* instance();
     void adjustCards();
     void addCard(Card* card, bool face = true, bool stand = true);
     Card* takeCard(int index);
     QList<Card*> getYourFieldyard() const;
-
     void response_addCard(Card* card, bool face = true, bool stand = true);
     Card* response_takeCard(int index);
 
 private:
-    QPixmap pixmap;
     QList<Card*> yourFieldyard;
 };
 
-class EnemyFieldgroundArea : public QGraphicsObject
+class EnemyFieldgroundArea : public QObject, public QGraphicsPixmapItem
 {
     Q_OBJECT
 public:
-    EnemyFieldgroundArea();
-    QRectF boundingRect() const;
-    void paint(QPainter* painter, const QStyleOptionGraphicsItem*, QWidget*);
+    static EnemyFieldgroundArea* instance();
     QList<Card*> getYourFieldground() const;
-
     //    void response_addCard(Card* card);
     //    Card* response_takeCard(int index);
-
 private:
-    QPixmap pixmap;
     QList<Card*> yourFieldground;
 };
 
-class EnemyGraveyardArea : public QGraphicsObject
+class EnemyGraveyardArea : public QObject, public QGraphicsPixmapItem
 {
     Q_OBJECT
 public:
-    EnemyGraveyardArea();
-    QRectF boundingRect() const;
-    void paint(QPainter* painter, const QStyleOptionGraphicsItem*, QWidget*);
+    static EnemyGraveyardArea* instance();
     void adjustCards();
     void addCard(Card* card);
     Card* takeCard(int index);
     QList<Card*> getYourGraveyard() const;
-
     void response_addCard(Card* card);
     Card* response_takeCard(int index);
 
 private:
-    QPixmap pixmap;
     QList<Card*> yourGraveyard;
 };
 
