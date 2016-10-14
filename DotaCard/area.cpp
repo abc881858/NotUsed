6a8471c2fc175272321843f4dfba74f0ff04ac4b
吴冬亮 <abc8333@gmail.com>
2016-10-14 17:40:47 +0800

    Update area.cpp

--- DotaCard/area.cpp
+++ DotaCard/area.cpp
@@ -1,20 +1,70 @@
-#include <QPainter>
+#include "area.h"
+
 #include <QDebug>
 
 #include "net.h"
-#include "area.h"
 #include "card.h"
 
-static QRectF DeckAreaRect(0, 0, 50, 72);
-static QRectF HandAreaRect(0, 0, 512, 70);
-static QRectF FieldyardAreaRect(0, 0, 384, 92);
-static QRectF FieldgroundAreaRect(0, 0, 384, 92);
-static QRectF GraveyardAreaRect(0, 0, 50, 72);
-static QRectF EnemyDeckAreaRect(0, 0, 50, 72);
-static QRectF EnemyHandAreaRect(0, 0, 512, 70);
-static QRectF EnemyFieldyardAreaRect(0, 0, 384, 92);
-static QRectF EnemyFieldgroundAreaRect(0, 0, 384, 92);
-static QRectF EnemyGraveyardAreaRect(0, 0, 50, 72);
+Q_GLOBAL_STATIC(DeckArea, deckArea)
+Q_GLOBAL_STATIC(HandArea, handArea)
+Q_GLOBAL_STATIC(FieldyardArea, fieldyardArea)
+Q_GLOBAL_STATIC(FieldgroundArea, fieldgroundArea)
+Q_GLOBAL_STATIC(GraveyardArea, graveyardArea)
+Q_GLOBAL_STATIC(EnemyDeckArea, enemyDeckArea)
+Q_GLOBAL_STATIC(EnemyHandArea, enemyHandArea)
+Q_GLOBAL_STATIC(EnemyFieldyardArea, enemyFieldyardArea)
+Q_GLOBAL_STATIC(EnemyFieldgroundArea, enemyFieldgroundArea)
+Q_GLOBAL_STATIC(EnemyGraveyardArea, enemyGraveyardArea)
+
+DeckArea* DeckArea::instance()
+{
+    return deckArea();
+}
+
+HandArea* HandArea::instance()
+{
+    return handArea();
+}
+
+FieldyardArea* FieldyardArea::instance()
+{
+    return fieldyardArea();
+}
+
+FieldgroundArea* FieldgroundArea::instance()
+{
+    return fieldgroundArea();
+}
+
+GraveyardArea* GraveyardArea::instance()
+{
+    return graveyardArea();
+}
+
+EnemyDeckArea* EnemyDeckArea::instance()
+{
+    return enemyDeckArea();
+}
+
+EnemyHandArea* EnemyHandArea::instance()
+{
+    return enemyHandArea();
+}
+
+EnemyFieldyardArea* EnemyFieldyardArea::instance()
+{
+    return enemyFieldyardArea();
+}
+
+EnemyFieldgroundArea* EnemyFieldgroundArea::instance()
+{
+    return enemyFieldgroundArea();
+}
+
+EnemyGraveyardArea* EnemyGraveyardArea::instance()
+{
+    return enemyGraveyardArea();
+}
 
 ///////////////////////////////////////////////////////////////
 /**
@@ -22,21 +72,6 @@ static QRectF EnemyGraveyardAreaRect(0, 0, 50, 72);
   * @author wudongliang
   * @date 2016/4/22
   */
-
-DeckArea::DeckArea()
-    : pixmap(":/png/png/temp.png")
-{
-}
-
-QRectF DeckArea::boundingRect() const
-{
-    return DeckAreaRect;
-}
-
-void DeckArea::paint(QPainter* painter, const QStyleOptionGraphicsItem*, QWidget*)
-{
-    painter->drawPixmap(0, 0, pixmap);
-}
 
 void DeckArea::addCard(Card* card)
 {
@@ -72,21 +107,6 @@ QList<Card*> DeckArea::getMyDeck() const
   * @author wudongliang
   * @date 2016/4/22
   */
-
-HandArea::HandArea()
-    : pixmap(":/png/png/hand.png")
-{
-}
-
-QRectF HandArea::boundingRect() const
-{
-    return HandAreaRect;
-}
-
-void HandArea::paint(QPainter* painter, const QStyleOptionGraphicsItem*, QWidget*)
-{
-    painter->drawPixmap(0, 0, pixmap);
-}
 
 void HandArea::adjustCards()
 {
@@ -133,21 +153,6 @@ QList<Card*> HandArea::getMyHand() const
   * @author wudongliang
   * @date 2016/9/2
   */
-
-FieldyardArea::FieldyardArea()
-    : pixmap(":/png/png/field.png")
-{
-}
-
-QRectF FieldyardArea::boundingRect() const
-{
-    return FieldyardAreaRect;
-}
-
-void FieldyardArea::paint(QPainter* painter, const QStyleOptionGraphicsItem*, QWidget*)
-{
-    painter->drawPixmap(0, 0, pixmap);
-}
 
 void FieldyardArea::initializeCards()
 {
@@ -203,21 +208,6 @@ QList<Card*> FieldyardArea::getMyFieldyard() const
   * @date 2016/9/2
   */
 
-FieldgroundArea::FieldgroundArea()
-    : pixmap(":/png/png/field.png")
-{
-}
-
-QRectF FieldgroundArea::boundingRect() const
-{
-    return FieldgroundAreaRect;
-}
-
-void FieldgroundArea::paint(QPainter* painter, const QStyleOptionGraphicsItem*, QWidget*)
-{
-    painter->drawPixmap(0, 0, pixmap);
-}
-
 QList<Card*> FieldgroundArea::getMyFieldground() const
 {
     return myFieldground;
@@ -229,21 +219,6 @@ QList<Card*> FieldgroundArea::getMyFieldground() const
   * @author wudongliang
   * @date 2016/9/2
   */
-
-GraveyardArea::GraveyardArea()
-    : pixmap(":/png/png/temp.png")
-{
-}
-
-QRectF GraveyardArea::boundingRect() const
-{
-    return GraveyardAreaRect;
-}
-
-void GraveyardArea::paint(QPainter* painter, const QStyleOptionGraphicsItem*, QWidget*)
-{
-    painter->drawPixmap(0, 0, pixmap);
-}
 
 void GraveyardArea::addCard(Card* card)
 {
@@ -288,21 +263,6 @@ void GraveyardArea::adjustCards()
   * @author wudongliang
   * @date 2016/4/22
   */
-
-EnemyDeckArea::EnemyDeckArea()
-    : pixmap(":/png/png/temp.png")
-{
-}
-
-QRectF EnemyDeckArea::boundingRect() const
-{
-    return EnemyDeckAreaRect;
-}
-
-void EnemyDeckArea::paint(QPainter* painter, const QStyleOptionGraphicsItem*, QWidget*)
-{
-    painter->drawPixmap(0, 0, pixmap);
-}
 
 void EnemyDeckArea::addCard(Card* card)
 {
@@ -359,21 +319,6 @@ QList<Card*> EnemyDeckArea::getYourDeck() const
   * @author wudongliang
   * @date 2016/4/22
   */
-
-EnemyHandArea::EnemyHandArea()
-    : pixmap(":/png/png/hand.png")
-{
-}
-
-QRectF EnemyHandArea::boundingRect() const
-{
-    return EnemyHandAreaRect;
-}
-
-void EnemyHandArea::paint(QPainter* painter, const QStyleOptionGraphicsItem*, QWidget*)
-{
-    painter->drawPixmap(0, 0, pixmap);
-}
 
 void EnemyHandArea::adjustCards()
 {
@@ -434,21 +379,6 @@ Card* EnemyHandArea::response_takeCard(int index)
   * @author wudongliang
   * @date 2016/9/2
   */
-
-EnemyFieldyardArea::EnemyFieldyardArea()
-    : pixmap(":/png/png/field.png")
-{
-}
-
-QRectF EnemyFieldyardArea::boundingRect() const
-{
-    return EnemyFieldyardAreaRect;
-}
-
-void EnemyFieldyardArea::paint(QPainter* painter, const QStyleOptionGraphicsItem*, QWidget*)
-{
-    painter->drawPixmap(0, 0, pixmap);
-}
 
 void EnemyFieldyardArea::addCard(Card* card, bool face, bool stand)
 {
@@ -511,21 +441,6 @@ void EnemyFieldyardArea::adjustCards()
   * @date 2016/9/2
   */
 
-EnemyFieldgroundArea::EnemyFieldgroundArea()
-    : pixmap(":/png/png/field.png")
-{
-}
-
-QRectF EnemyFieldgroundArea::boundingRect() const
-{
-    return EnemyFieldgroundAreaRect;
-}
-
-void EnemyFieldgroundArea::paint(QPainter* painter, const QStyleOptionGraphicsItem*, QWidget*)
-{
-    painter->drawPixmap(0, 0, pixmap);
-}
-
 QList<Card*> EnemyFieldgroundArea::getYourFieldground() const
 {
     return yourFieldground;
@@ -537,21 +452,6 @@ QList<Card*> EnemyFieldgroundArea::getYourFieldground() const
   * @author wudongliang
   * @date 2016/9/2
   */
-
-EnemyGraveyardArea::EnemyGraveyardArea()
-    : pixmap(":/png/png/temp.png")
-{
-}
-
-QRectF EnemyGraveyardArea::boundingRect() const
-{
-    return EnemyGraveyardAreaRect;
-}
-
-void EnemyGraveyardArea::paint(QPainter* painter, const QStyleOptionGraphicsItem*, QWidget*)
-{
-    painter->drawPixmap(0, 0, pixmap);
-}
 
 void EnemyGraveyardArea::addCard(Card* card)
 {
