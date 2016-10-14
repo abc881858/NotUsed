5dd9c6b3030df1cc69cce76af1de76b8c249169e
吴冬亮 <abc8333@gmail.com>
2016-10-14 17:49:38 +0800

    Update mainwindow.cpp

--- DotaCard/mainwindow.cpp
+++ DotaCard/mainwindow.cpp
@@ -4,10 +4,12 @@
 #include <QBrush>
 #include <QPalette>
 #include <QPixmap>
+#include <QCursor>
 
 #include "net.h"
 #include "roomscene.h"
 #include "rule.h"
+#include "card.h"
 
 MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent)
@@ -21,6 +23,9 @@ MainWindow::MainWindow(QWidget* parent)
     Net::instance()->initialize();
     Rule::instance()->initialize();
 
+    QCursor cursorCommmon = QCursor(QPixmap(":/png/png/3.cur"), 31, 15);
+    setCursor(cursorCommmon);
+
     myLP = 8000;
     yourLP = 8000;
     ui->myLcdNumber->display(myLP);
@@ -30,33 +35,37 @@ MainWindow::MainWindow(QWidget* parent)
     ui->roomView->setScene(roomScene);
     ui->roomView->setSceneRect(0, 0, 550, 600);
 
-    connect(roomScene, &RoomScene::hover, [=](QString name, QString description) {
-        //首先一张卡被hover，左上角大图肯定要更新
-        //但对方的（除前后场的face-up卡）不该显示，我方卡组也不该显示
-        ui->label->setStyleSheet(QString("image: url(:/pic/monster/%1.jpg)").arg(name));
-        ui->textEdit->setText(description);
-    });
-    connect(ui->buttonBP, &QPushButton::pressed, [=]() {
-        if (Rule::instance()->getphase() == Rule::myM1)
+    connect(roomScene, &RoomScene::hover, [=](QString name, QString description)
         {
-            Rule::instance()->setPhase(Rule::myBP);
-            Rule::instance()->setDoing(false);
-        }
-    });
-    connect(ui->buttonM2, &QPushButton::pressed, [=]() {
-        if (Rule::instance()->getphase() == Rule::myBP)
+            //首先一张卡被hover，左上角大图肯定要更新
+            //但对方的（除前后场的face-up卡）不该显示，我方卡组也不该显示
+            ui->label->setStyleSheet(QString("image: url(:/pic/monster/%1.jpg)").arg(name));
+            ui->textEdit->setText(description);
+        });
+    connect(ui->buttonBP, &QPushButton::pressed, [=]()
         {
-            Rule::instance()->setPhase(Rule::myM2);
-            Rule::instance()->setDoing(false);
-        }
-    });
-    connect(ui->buttonEP, &QPushButton::pressed, [=]() {
-        if (Rule::instance()->getphase() == Rule::myM1 || Rule::instance()->getphase() == Rule::myM2)
+            if (Rule::instance()->getphase() == Rule::myM1)
+            {
+                Rule::instance()->setPhase(Rule::myBP);
+                Rule::instance()->setDoing(false);
+            }
+        });
+    connect(ui->buttonM2, &QPushButton::pressed, [=]()
         {
-            Rule::instance()->setPhase(Rule::myEP);
-            Rule::instance()->setDoing(false);
-        }
-    });
+            if (Rule::instance()->getphase() == Rule::myBP)
+            {
+                Rule::instance()->setPhase(Rule::myM2);
+                Rule::instance()->setDoing(false);
+            }
+        });
+    connect(ui->buttonEP, &QPushButton::pressed, [=]()
+        {
+            if (Rule::instance()->getphase() == Rule::myM1 || Rule::instance()->getphase() == Rule::myM2)
+            {
+                Rule::instance()->setPhase(Rule::myEP);
+                Rule::instance()->setDoing(false);
+            }
+        });
 }
 
 MainWindow::~MainWindow()
